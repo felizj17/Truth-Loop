f6df7b8d26c78280b9fcf65fc53696ee19f8aa9a
Juan Feliz <felizj17@gmail.com>
2021-10-29 01:35:42 -0400

    Route format
    Signed-off-by: Juan Feliz <felizj17@gmail.com>

--- server/react-admin/src/AdminApp.js
+++ server/react-admin/src/AdminApp.js
@@ -16,17 +16,17 @@ function AdminApp() {
       <div id={'app'}>
         <div id={'main-content'} data-testid={'mainContent'}>
           <Switch>
-            <Route name="admin-home-route" exact path={'/'} component={AdminHomeRoute} />
-            <Route name="publication-route" exact path={'/publications'} component={PublicationRoute} />
+			<Route name="admin-home-route" exact path={'/'} component={AdminHomeRoute} />
+			<Route name="publication-route" exact path={'/publications'} component={PublicationRoute} />
 			<Route name="video-testimonial-route" exact path={'/videoTestimonials'} component={VideoTestimonialRoute} />
 			<Route name="level-route" exact path={'/levels'} component={LevelsRoute} />
 			<Route name="add-testimonial-form" exact path={'/videoTestimonials/add'} component={VideoTestimonialForm}/>
 			<Route name="edit-testimonial-form" path={'/videoTestimonials/edit/:id'} component={VideoTestimonialForm} />
 			<Route name="advocacy-group-route" exact path={'/advocacyGroups'} component={AdvocacyGroupsRoute}/>
-            <Route name="add-publication-form" exact path={'/publications/add'} component={PublicationForm}/>
-            <Route name="edit-publication-form" path={'/publications/edit/:id'} component={PublicationForm}/>
+			<Route name="add-publication-form" exact path={'/publications/add'} component={PublicationForm}/>
+			<Route name="edit-publication-form" path={'/publications/edit/:id'} component={PublicationForm}/>
 			<Route name="add-advocacy-group-form" exact path={'/advocacyGroups/add'} component={AdvocacyGroupsForm} />
-            <Route name="edit-advocacy-group-form" path={'/advocacyGroups/edit/:id'} component={AdvocacyGroupsForm} />
+			<Route name="edit-advocacy-group-form" path={'/advocacyGroups/edit/:id'} component={AdvocacyGroupsForm} />
 			<Route name="add-level-form" exact path={'/levels/add'} component={LevelsForm}/>
 			<Route name="edit-level-form" path={'/levels/edit/:id'} component={LevelsForm}/>
           </Switch>
